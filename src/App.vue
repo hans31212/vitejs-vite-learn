ba5da49ebf6bef07728db83348e901f2c0119644
hans31212 <hans.yang@bsd.ipeka.sch.id>
2022-08-25 13:04:02 +0700

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 import todo from './components/todoItem.vue'
 
+
 function todoItem(done=false, content='', children=[]) {
   return {
     "done" : done,
@@ -26,7 +27,7 @@ const todoList = [
   >
     <todo 
     v-for="item in todoList"
-    :key="item.id"
+    :key="(Math.random().toString(36).substring(2))"
     :todo="item"
     />
   </div>
