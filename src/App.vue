26df843edc7cc4b759e3fd34056a6948cc909a64
hans31212 <hans.yang@bsd.ipeka.sch.id>
2022-08-25 08:55:19 +0700

    2 files modified, 1 added, 1 deleted

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
+import todo from './components/todoItem.vue'
 
+function todoItem(done=false, content='', children=[]) {
+  return {
+    "done" : done,
+    "content" : content,
+    "children" : children
+  }
+}
+
+const todoList = [
+  todoItem(false, 'Create todoApp', [
+    todoItem(true, 'Setup environment', [
+      todoItem(true, 'Install Vue'),
+      todoItem(true, 'Install Tailwind')
+    ]),
+    todoItem(false, 'Create TodoItem Component')
+  ])
+]
 </script>
 <template>
+  <div 
+  class="flex flex-col w-full items-center p-4"
+  >
+    <todo 
+    v-for="item in todoList"
+    :key="item.id"
+    :todo="item"
+    />
+  </div>
 </template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  // These paths are just examples, customize them to match your project structure
-  purge: [
-    "./index.html",
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
